2a1705b3d3338952169eac577bc3f7a9f3e3ff8c
Gaurav Gola <gauravsom443@gmail.com>
2026-06-04 09:38:48 +0530

    Responsive navbar and filters UI

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config({path:"../.env"});
 const mongoose =require("mongoose");
 const initdata=require("./data.js");
 const Listing=require("../models/listing.js");
+const dbUrl=process.env.ATLASDB_URL;
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 main()
 .then(()=>{
     console.log("connected to DB");
@@ -11,13 +13,13 @@ main()
     console.log(err);
 });
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 
 const initDb=async ()=>{
     await Listing.deleteMany({});
-    initdata.data=initdata.data.map((obj)=>({...obj,owner:"6a147c517035be29c66c16f8"})); 
+    initdata.data=initdata.data.map((obj)=>({...obj,owner:"6a1fae8169bd0e87d20effd9",geometry:{type:"Point",coordinates:[77.1025,28.7041]}})); 
     await Listing.insertMany(initdata.data);
     console.log("data was saved");
 
